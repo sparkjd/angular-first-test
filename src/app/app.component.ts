8945d9f6bdc139303cd78bfaad8d72c8e899e29f
spark <spark.justdev@gmail.com>
2019-01-26 16:29:22 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'first-angular';
+  title = 'first-angular-test';
+
+  getTestText() {
+    return 'test Text function executed';
+  }
 }
